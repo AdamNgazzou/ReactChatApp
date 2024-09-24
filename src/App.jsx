d46c7595521c73fc351a50acdc229cade230abe3
AdamNgazzou <117188411+AdamNgazzou@users.noreply.github.com>
2024-09-24 23:30:23 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
-import List from "./components/list/list";
+import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 import { useUserStore } from "./lib/userStore";
 import { useChatStore } from "./lib/chatStore";
+import { useUiStore } from "./lib/uiStore";
 const App = () => {
 
   const { currentUser, isLoading, fetchUserInfo } = useUserStore();
@@ -29,11 +30,14 @@ const App = () => {
       unSub();
     };
   }, [fetchUserInfo]);
+  const { chatorchatlist } = useUiStore();
+  const mediaQuery = window.matchMedia("(max-width: 768px)");
+
 
   console.log(currentUser);
   if (isLoading) return <div className="loading">Loading...</div>
   return (
-    <div className='container'>
+    <div className='container' style={{ backgroundColor: (chatorchatlist && mediaQuery.matches) ? 'none' : 'rgb(17,25,40,0.75)', backdropFilter: (chatorchatlist && mediaQuery.matches) ? 'none' : 'blur(19px) saturate(180%)' }}>
       {
         currentUser ? (
           <>
