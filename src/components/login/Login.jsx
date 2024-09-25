b7a76ad557cec5a6df87bcb3094fc284c4d7c339
AdamNgazzou <117188411+AdamNgazzou@users.noreply.github.com>
2024-09-25 18:25:09 +0100

    Update Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react'
 import './login.css'
 import { toast } from 'react-toastify'
@@ -5,6 +6,7 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'fire
 import { auth, db } from '../../lib/firebase'
 import { doc, setDoc } from 'firebase/firestore'
 import upload from '../../lib/upload'
+import { useLoginorregisterStore } from '../../lib/loginStore'
 const Login = () => {
     const [avatar, setAvatar] = useState({
         file: null,
@@ -24,6 +26,8 @@ const Login = () => {
     }
 
 
+    const { Loginorregister, setLoginorregister } = useLoginorregisterStore();
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
 
 
     const handleRegister = async (e) => {
@@ -77,18 +81,22 @@ const Login = () => {
             setLoading(false);
         }
     };
+    console.log(mediaQuery);
+    console.log(Loginorregister)
     return (
         <div className='login'>
-            <div className="item">
+            <div className="item" style={{ display: (!Loginorregister && mediaQuery.matches) ? 'none' : 'flex' }}>
                 <h2>Welcome back,</h2>
                 <form onSubmit={handleLogin} >
                     <input type="email" placeholder='Email' name="email" required />
                     <input type="password" placeholder='Password' name="password" required />
+                    <p style={{ display: !mediaQuery.matches ? 'none' : 'flex' }}>I don't have an account <span className='linki' onClick={() => setLoginorregister(!Loginorregister)}>Click Here</span></p>
+
                     <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
                 </form>
             </div>
-            <div className="separator"></div>
-            <div className="item">
+            <div className="separator" style={{ display: !mediaQuery ? 'none' : 'flex' }}></div>
+            <div className="item" style={{ display: (Loginorregister && !mediaQuery.matches) ? 'none' : 'flex' }}>
                 <h2>Create an Account,</h2>
                 <form onSubmit={handleRegister}>
                     <label htmlFor="file"><img src={avatar.url || "./avatar.png"} alt="" required />Upload an image</label>
@@ -96,6 +104,7 @@ const Login = () => {
                     <input type="text" placeholder='Username' name="username" required />
                     <input type="email" placeholder='Email' name="email" required />
                     <input type="password" placeholder='Password' name="password" required />
+                    <p style={{ display: !mediaQuery.matches ? 'none' : 'flex' }}>i already have an account <span className='linki' onClick={() => setLoginorregister(!Loginorregister)}>Click Here</span></p>
                     <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
                 </form>
             </div>
